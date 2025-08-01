33fdf036491071d352da22521a45653c6d4edba3
ellavilleza22@gmail.com <ellavilleza22@gmail.com>
2025-08-01 22:27:02 +0800

    part12-1:20:45

--- client/src/components/button/CloseButton.tsx
+++ client/src/components/button/CloseButton.tsx
@@ -1,33 +1,52 @@
 import type { FC } from "react";
-import { Link } from "react-router-dom";
 
-interface BackButtonProps {
+interface CloseButtonProps {
   label: string;
-  path: string;
-  newClassName?: string;
+  onClose: () => void;
   className?: string;
 }
 
-const BackButton: FC<BackButtonProps> = ({
-  label,
-  path,
-  newClassName,
-  className,
-}) => {
+const CloseButton: FC<CloseButtonProps> = ({ label, onClose, className }) => {
   return (
-    <>
-      <Link
-        to={path}
-        className={`${
-          newClassName
-            ? newClassName
-            : `px-4 py-3 bg-white hover:bg-gray-100 text-gray-600 hover:text-gray-700 text-sm font-medium cursor-pointer rounded-lg shadow-lg ${className}`
-        }`}
-      >
-        {label}
-      </Link>
-    </>
+    <button
+      type="button"
+      onClick={onClose}
+      className={`px-4 py-3 bg-white hover:bg-gray-100 text-gray-600 hover:text-gray-700 text-sm font-medium cursor-pointer rounded-lg shadow-lg ${
+        className ?? ""
+      }`}
+    >
+      {label}
+    </button>
   );
 };
 
-export default BackButton;
+// interface CloseButtonProps {
+//   label: string;
+//   path: string;
+//   newClassName?: string;
+//   className?: string;
+// }
+
+// const CloseButton: FC<CloseButtonProps> = ({
+//   label,
+//   path,
+//   newClassName,
+//   className,
+// }) => {
+//   return (
+//     <>
+//       <Link
+//         to={path}
+//         className={`${
+//           newClassName
+//             ? newClassName
+//             : `px-4 py-3 bg-white hover:bg-gray-100 text-gray-600 hover:text-gray-700 text-sm font-medium cursor-pointer rounded-lg shadow-lg ${className}`
+//         }`}
+//       >
+//         {label}
+//       </Link>
+//     </>
+//   );
+// };
+
+export default CloseButton;
--- client/src/pages/User/components/AddUserFormModal.tsx
+++ client/src/pages/User/components/AddUserFormModal.tsx
@@ -1,20 +1,126 @@
-import type { FC } from "react";
+import { useEffect, useState, type FC, type FormEvent } from "react";
 import FloatingLabelInput from "../../../components/input/FloatingLabelInput";
 import Modal from "../../../components/Modal";
 import FloatingLabelSelect from "../../../components/select/FloatingLabelSelect";
 import SubmitButton from "../../../components/button/SubmitButton";
 import CloseButton from "../../../components/button/CloseButton";
+import type { GenderColumns } from "../../../Interfaces/GenderColumns";
+import GenderService from "../../../services/GenderService";
+import type { UserFieldErrors } from "../../../Interfaces/UserFieldErrors";
+import UserService from "../../../services/UserServices";
 
 interface AddUserFormModalProps {
+  onUserAdded: (message: string) => void;
   isOpen: boolean;
   onClose: () => void;
 }
 
-const AddUserFormModal: FC<AddUserFormModalProps> = ({ isOpen, onClose }) => {
+const AddUserFormModal: FC<AddUserFormModalProps> = ({
+  onUserAdded,
+  isOpen,
+  onClose,
+}) => {
+  const [loadingGenders, setLoadingGenders] = useState(false);
+  const [genders, setGenders] = useState<GenderColumns[]>([]);
+
+  const [loadingStore, setLoadingStore] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [suffixName, setSuffixName] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState<UserFieldErrors>({});
+
+  const handleStoreUser = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+
+      setLoadingStore(true);
+
+      const payload = {
+        first_name: firstName,
+        middle_name: middleName,
+        last_name: lastName,
+        suffix_name: suffixName,
+        gender: gender,
+        birth_date: birthDate,
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      };
+
+      const res = await UserService.storeUser(payload);
+
+      if (res.status === 200) {
+        onUserAdded(res.data.message);
+
+        setFirstName("");
+        setMiddleName("");
+        setLastName("");
+        setSuffixName("");
+        setGender("");
+        setBirthDate("");
+        setUsername("");
+        setPassword("");
+        setPasswordConfirmation("");
+        setErrors({});
+      } else {
+        console.error(
+          "Unexpected status error occured during adding user:",
+          res.status
+        );
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error.response && error.response === 422) {
+        setErrors(error.response.data.errors);
+      } else {
+        console.log(
+          "Unexpected server error occured during adding user: ",
+          error
+        );
+      }
+    } finally {
+      setLoadingStore(false);
+    }
+  };
+
+  const handleLoadGenders = async () => {
+    try {
+      setLoadingGenders(true);
+
+      const res = await GenderService.loadGenders();
+
+      if (res.status === 200) {
+        setGenders(res.data.genders);
+      } else {
+        console.error(
+          "Unexpected status error occurred during loading genders: ",
+          res.status
+        );
+      }
+    } catch (error) {
+      console.error(
+        "Unexpected server error occurred during loading genders: ",
+        error
+      );
+    } finally {
+      setLoadingGenders(false);
+    }
+  };
+
+  useEffect(() => {
+    handleLoadGenders();
+  }, []);
+
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose} showCloseButton>
-        <form>
+        <form onSubmit={handleStoreUser}>
           <h1 className="text-2xl border-b border-gray-100 p-4 font-semibold mb-4">
             Add User Form
           </h1>
@@ -23,82 +129,122 @@ const AddUserFormModal: FC<AddUserFormModalProps> = ({ isOpen, onClose }) => {
               <div className="mb-4">
                 <FloatingLabelInput
                   label="First Name"
-                  name="first_name"
                   type="text"
+                  name="first_name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
                   required
                   autoFocus
+                  errors={errors.first_name}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Middle Name"
-                  name="middle_name"
                   type="text"
+                  name="middle_name"
+                  value={middleName}
+                  onChange={(e) => setMiddleName(e.target.value)}
+                  errors={errors.middle_name}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Last Name"
-                  name="last_name"
                   type="text"
+                  name="last_name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                   required
+                  errors={errors.last_name}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Suffix Name"
-                  name="suffix_name"
                   type="text"
+                  name="suffix_name"
+                  value={suffixName}
+                  onChange={(e) => setSuffixName(e.target.value)}
+                  errors={errors.suffix_name}
                 />
               </div>
               <div className="mb-4">
-                <FloatingLabelSelect label="Gender" name="gender">
-                  {genders.map((gender, index) => (
-                    <option value={gender.gender_id} key={index}>
-                      {gender.gender}
-                    </option>
-                  ))}
+                <FloatingLabelSelect
+                  label="Gender"
+                  name="gender"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                  errors={errors.gender}
+                >
+                  <option value="">Select Gender</option>
+                  {loadingGenders ? (
+                    <option value="">Loading...</option>
+                  ) : (
+                    genders.map((gender, index) => (
+                      <option value={gender.gender_id} key={index}>
+                        {gender.gender}
+                      </option>
+                    ))
+                  )}
                 </FloatingLabelSelect>
               </div>
             </div>
             <div className="cols-span-2 md:col-span-1">
               <div className="mb-4">
                 <FloatingLabelInput
-                  label="Birth hdate"
-                  name="birth_date"
+                  label="Birth date"
                   type="date"
+                  name="birth_date"
+                  value={birthDate}
+                  onChange={(e) => setBirthDate(e.target.value)}
                   required
+                  errors={errors.birth_date}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Username"
-                  name="username"
                   type="text"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   required
+                  errors={errors.username}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Password"
-                  name="password"
                   type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
+                  errors={errors.password}
                 />
               </div>
               <div className="mb-4">
                 <FloatingLabelInput
                   label="Password Confirmation"
-                  name="password_confirmation"
                   type="password"
+                  name="password_confirmation"
+                  value={passwordConfirmation}
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
                   required
+                  errors={errors.password_confirmation}
                 />
               </div>
             </div>
           </div>
           <div className="flex justify-end gap-2">
-            <CloseButton label="Close" onClose={onClose} />
-            <SubmitButton label="Save User" />
+            {!loadingStore && <CloseButton label="Close" onClose={onClose} />}
+            <SubmitButton
+              label="Save User"
+              loading={loadingStore}
+              loadingLabel="Saving User..."
+            />
           </div>
         </form>
       </Modal>
